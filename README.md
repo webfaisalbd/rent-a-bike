14270192f651d01968109be0a57fa253dfb5adec
webfaisalbd <web.faisal.bd@gmail.com>
2023-01-21 20:45:40 +0600

    scroll animation added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Rent-A-Bike
+
+Note:
+ - responsive website
+ - using boxicons for icon
+ - using scrollreveal for scroll animation of the content in website
+
+
+
+#### website demo:
+
+<p align='center'>
+  <img src="./img/website-demo.png" >
+</p>
--- main.js
+++ main.js
@@ -5,3 +5,23 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+// scrollreveal 
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{ delay: 200, origin: 'top' })
+sr.reveal('.form-container form', { delay: 800, origin: 'left' })
+sr.reveal('.heading', { delay: 800, origin: 'top' })
+sr.reveal('.ride-container .box', { delay: 400, origin: 'top' })
+sr.reveal('.services-container .box', { delay: 400, origin: 'top' })
+sr.reveal('.about-description', { delay: 400, origin: 'right' })
+sr.reveal('.about-img', { delay: 400, origin: 'left' })
+sr.reveal('.reviews-container .single-review', { delay: 400, origin: 'top' })
+sr.reveal('.newsLetter-container', { delay: 400, origin: 'right' })
+sr.reveal('.copyright-icons .bx', { delay: 800, origin: 'left' })
